e9ca8becb4a89224106f745c7c25b582c9d2ea98
studioele <71141068+studioele@users.noreply.github.com>
2022-11-14 23:09:26 +0100

    1

--- brew_install_apps.sh
+++ brew_install_apps.sh
@@ -3,66 +3,62 @@
 # Homebrew Script for OSX
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
-# echo "Installing brew..."
-# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "Installing Command Line Tools (CLT) for Xcode"
+xcode-select --install 
 
-# echo "Installing brew cask..."
-# brew tap homebrew/cask
+echo "Installing brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Programming Languages
-# echo "Installing programming languages..."
-# brew install scala
-# brew install --cask r
-# brew install --cask oracle-jdk-javadoc
+echo "Installing brew cask..."
+brew tap homebrew/cask
 
 # # Dev Tools
 echo "Installing development tools..."
-# brew install docker
-# brew install git
-# brew install basex
-# brew install --cask github
-# brew install --cask hyper
-# brew install --cask kitematic
-# brew install --cask neo4j
-# brew install --cask rstudio
-brew install --cask visual-studio-code
+brew install --cask --no-quarantine github
+brew install --cask --no-quarantine visual-studio-code
+brew install --cask --no-quarantine iterm2
 
 # # Communication Apps
-# echo "Installing communication apps..."
-# brew install --cask discord
-# brew install --cask keybase
-# brew install --cask microsoft-teams
-# brew install --cask microsoft-outlook
-# brew install --cask protonmail-bridge
-# brew install --cask skype
-# brew install --cask slack
-# brew install --cask zoom
+echo "Installing communication apps..."
+brew install --cask --no-quarantine microsoft-teams
+brew install --cask --no-quarantine skype
+brew install --cask --no-quarantine zoom
+brew install --cask --no-quarantine anydesk
 
 # # Web Tools
-# echo "Installing web tools..."
-# brew install httpie
-# brew install node
-# brew install nvm
-# brew install --cask firefox
-# brew install --cask google-chrome
-# brew install --cask ngrok
-# brew install --cask postman
+echo "Installing web tools..."
+brew install --cask --no-quarantine firefox
+brew install --cask --no-quarantine google-chrome
 
 # # File Storage
-# echo "Installing file storage tools..."
-# brew install --cask dropbox
-# brew install --cask onedrive
+echo "Installing file storage tools..."
+brew install --cask --no-quarantine google-drive
 
 # # Writing Apps
-# echo "Installing writing apps..."
-# brew install pandoc
-# brew install --cask deepl
-# brew install --cask zotero
-# brew install --cask papers
-# brew install --cask microsoft-word
+echo "Installing writing apps..."
+brew install --cask --no-quarantine libreoffice
+brew install --cask --no-quarantine libreoffice-language-pack
+    
+# # Graphics
+echo "Installing graphics apps..."
+brew install --cask --no-quarantine gimp
+brew install --cask --no-quarantine blender
+brew install --cask --no-quarantine inkscape
+brew install --cask --no-quarantine scribus
+brew install --cask --no-quarantine qgis
+brew install --cask --no-quarantine darktable
 
-# # Other
-# echo "Installing everything else..."
-# brew install --cask anki
-# brew install --cask amazon-music
-# brew install --cask minecraft
+# # anything else
+echo "Installing anything else..."
+brew install --cask --no-quarantine appcleaner
+brew install --cask --no-quarantine macpass
+brew install --cask --no-quarantine keka
+brew install --cask --no-quarantine lulu
+
+
+
+# brew install --cask --no-quarantine google-drive gimp appcleaner macpass blender inkscape scribus firefox keka qgis libreoffice libreoffice-language-pack lulu darktable anydesk iterm2 zoom skype github
+
+# **PRELIMINARY**
+# // Install Xcode Tools:     xcode-select --install
+# // Install Homebrew:        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
